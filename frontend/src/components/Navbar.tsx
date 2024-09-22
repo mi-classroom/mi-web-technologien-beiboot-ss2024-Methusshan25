9830c8e1ccc579f70f8e24c0d9e110532a891a94
Methusshan Elankumaran <56478334+Methusshan25@users.noreply.github.com>
2024-09-22 17:15:34 +0200

    chore: remove unnecessary elements

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,19 +1,12 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 
-const drawerWidth = 240;
 
 function Navbar() {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -24,21 +17,6 @@ function Navbar() {
           <Button sx={{ position: 'absolute', right: 10, top: 15, borderRadius: 3 }} variant='contained' color='secondary' href="/projects">Projects</Button>
         </Toolbar>
       </AppBar>
-      <nav>
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-        </Drawer>
-      </nav>
       <Box component="main" sx={{ p: 3 }}>
         <Toolbar />
       </Box>
